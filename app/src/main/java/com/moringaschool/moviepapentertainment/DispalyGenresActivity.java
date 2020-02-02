6d63e2064f140f58e58ac39989dee32eabf047e2
omoshthegeek254 <omoshthegeek@gmail.com>
2020-02-03 00:53:26 +0300

    added the activity for displaying genres

---
+++ app/src/main/java/com/moringaschool/moviepapentertainment/DispalyGenresActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.moviepapentertainment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DispalyGenresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispaly_genres);
+    }
+}

5883292a6105883a9fa15a5f00eb6e080b2f5cf3
omoshthegeek254 <omoshthegeek@gmail.com>
2020-02-03 00:04:46 +0300

    added the main activity master codes

--- app/src/main/java/com/moringaschool/moviepapentertainment/MainActivity.java
+++ app/src/main/java/com/moringaschool/moviepapentertainment/MainActivity.java
@@ -3,12 +3,34 @@ package com.moringaschool.moviepapentertainment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.postGenresButton)
+    Button mPostRestaurantsButton;
+    @BindView(R.id.introTextView)
+    TextView mIntroTextView;
+    @BindView(R.id.introTitleTextView)
+    TextView mIntroTitleTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mPostRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

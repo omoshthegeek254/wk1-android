f30d2c09323633e29f14eea0daca08da185c2436
omoshthegeek254 <omoshthegeek@gmail.com>
2020-02-03 00:21:17 +0300

    added the main logic

--- app/src/main/java/com/moringaschool/moviepapentertainment/MainActivity.java
+++ app/src/main/java/com/moringaschool/moviepapentertainment/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.moviepapentertainment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         mPostRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostGenreActivity.class);
+                startActivity(intent);
 
             }
         });
